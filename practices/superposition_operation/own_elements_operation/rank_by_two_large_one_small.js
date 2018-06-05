b1f2a6b40bc3ae0a20c9ecb9ce5a1878fb6eb351
aimee9004 <826065426@qq.com>
2018-06-05 17:16:09 +0800

    rank_by_two... use splice

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,12 +1,14 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+  //这里写代码。。。 1 2 3 4 6 8 9 10 
+  //               2 3 1 6 8 4 9 10
   let arr = collection.sort(sToB)
-  // let newArr = []
-  // for(let i = 0; i < arr.length-arr.length%3; i++) {
-  //   newArr.push(i)
-  // }
-  // return newArr
+  let newArr = []
+  for(let i = 0; i < Math.floor(arr.length/3); i++) {
+    newArr = arr.splice(i*3, 1)
+    arr.splice((i+1)*3-1, 0, newArr[0])
+  }
+  return arr
 }
 function sToB(a, b) {
   return a - b
