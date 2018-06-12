6b219eba5388a416ace4d6fbef51cdf1d591ffaf
aimee9004 <826065426@qq.com>
2018-06-12 18:21:42 +0800

    最后一道题

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,18 +1,20 @@
 'use strict';
 var even_group_calculate_average = function(collection){
-    // 第偶数个元素
-    let evenArr = collection.filter((v, index) => {
-        return v % 2 !== 0
-    })
-
-    // 偶数数组
-    let evenNums = evenArr.filter(v => v % 2 === 0)
-    if(evenNums.length === 0) {
+    let sortArr = collection.filter((crr, idx) => idx % 2 !== 0 && crr % 2 === 0).sort((a, b) => a - b)
+    
+    let everyNum = 0, needArr = [];
+    if(sortArr.length === 0) {
         return [0]
-    }
-    let obj = {}
-    for(let i = 0; i < evenNums.length; i++) {
-        return evenNums[i]
+    }else {
+        return sortArr.reduce((acc, cur, idx, arr) => {
+            everyNum++
+            acc = acc + cur
+            if(idx < sortArr.length-1 && (String(arr[idx]).length != String(arr[idx+1]).length)) {    
+                needArr.push((acc + cur)/everyNum)
+                everyNum = 0
+            }
+            return needArr
+        }, [])
     }
 };
 
